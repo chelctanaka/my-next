a21eaad3e3fde46997cae3f73a5378834abe4972
chelctanaka <yuya_chelsea@yahoo.co.jp>
2024-05-16 23:49:38 +0900

    tanaka

--- src/app/books/[[...keyword]]/layout.js
+++ src/app/books/[[...keyword]]/layout.js
@@ -1,7 +1,7 @@
-'use client';
+"use client";
 
-import { useRouter } from 'next/navigation';
-import { useRef } from 'react';
+import { useRouter } from "next/navigation";
+import { useRef } from "react";
 
 export default function BooksLayout({ children }) {
   const router = useRouter();
@@ -12,15 +12,22 @@ export default function BooksLayout({ children }) {
 
   return (
     <>
-    <form className="mt-2 mb-4">
-      <input type="text" ref={txtKeyword}
-        className="bg-gray-100 text-black border border-gray-600 rounded mr-2 px-2 py-2 focus:bg-white focus:outline-none focus:border-red-500" />
-      <button type="button" onClick={handleSearch}
-        className="bg-blue-600 text-white rounded px-4 py-2 hover:bg-blue-500">
-        検索</button>
-    </form>
-    <hr />
-    {children}
+      <form className="mt-2 mb-4">
+        <input
+          type="text"
+          ref={txtKeyword}
+          className="bg-gray-100 text-black border border-gray-600 rounded mr-2 px-2 py-2 focus:bg-white focus:outline-none focus:border-red-500"
+        />
+        <button
+          type="button"
+          onClick={handleSearch}
+          className="bg-blue-600 text-white rounded px-4 py-2 hover:bg-blue-500"
+        >
+          検索
+        </button>
+      </form>
+      <hr />
+      {children}
     </>
   );
 }
